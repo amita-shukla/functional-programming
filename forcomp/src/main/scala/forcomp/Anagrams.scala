1e5b932b756d8ae35b2fc78f718a68b50cf4e4f0
Amita Shukla <amitashukla0906@gmail.com>
2016-12-21 05:32:29 +0530

    test mkString method"
    "

--- forcomp/src/main/scala/forcomp/Anagrams.scala
+++ forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -37,7 +37,7 @@ object Anagrams {
   def wordOccurrences(w: Word): Occurrences = w.toLowerCase().groupBy(c=> c).mapValues(s=>s.length).toList.sortBy(_._1)
 
   /** Converts a sentence into its character occurrence list. */
-  def sentenceOccurrences(s: Sentence): Occurrences = ???
+  def sentenceOccurrences(s: Sentence): Occurrences = s.
 
   /** The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
    *  the words that have that occurrence count.
--- forcomp/src/main/scala/forcomp/test.sc
+++ forcomp/src/main/scala/forcomp/test.sc
@@ -1,6 +1,10 @@
 val word : String = "Aabcb"
 word.toLowerCase().groupBy(c=> c).mapValues(s=>s.length).toList.sortBy(_._1)
 
+val sentence : List[String] = List("aaa","bbc","cdc")
+
+sentence.mkString("")
+
 
 
 
